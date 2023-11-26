272a1765efd550b3eb0bf48488857262308bab95
GunelD <gdibirova14113@ada.edu.az>
2023-11-26 23:04:31 +0400

    Continue to develop

--- script2.js
+++ script2.js
@@ -13,29 +13,28 @@ function showPageContent() {
             const card = document.createElement('div');
             card.setAttribute('class', 'card');
 
-            const title = document.createElement('h2');
-            title.textContent = product.title;
-
             const thumbnail = document.createElement('img');
             thumbnail.setAttribute('src', product.thumbnail);
 
+            const title = document.createElement('h2');
+            title.textContent = product.title.charAt(0).toUpperCase() + product.title.slice(1);
             const price = document.createElement('p');
-            price.textContent = product.price;
+            price.textContent = 'Price: ' + product.price + '$';
 
             const discountPercentage = document.createElement('p');
-            discountPercentage.textContent = product.discountPercentage + '%';
+            discountPercentage.textContent = 'Discount Percentage: ' + product.discountPercentage + '%';
 
             const category = document.createElement('p');
-            category.textContent = product.category;
+            category.textContent = 'Category: ' + product.category;
 
             const stock = document.createElement('p');
-            stock.textContent = product.stock;
+            stock.textContent = 'Current Stock: ' + product.stock;
 
 
             container.appendChild(card);
 
-            card.appendChild(title);
             card.appendChild(thumbnail);
+            card.appendChild(title);
             card.appendChild(price);
             card.appendChild(discountPercentage);
             card.appendChild(category);
@@ -43,8 +42,16 @@ function showPageContent() {
         });
     }
 
+    function handleFetchError() {
+        const error = document.createElement('p');
+        error.textContent = 'Error fetching data, please try again';
+        container.appendChild(error);
+    };
+
+
     fetch(url)
         .then(response => response.json())
         .then(data => showProducts(data.products))
         .catch(handleFetchError);
 };
+
