46c5f124ec94730ad01672f474277bf0e13f106b
GunelD <gdibirova14113@ada.edu.az>
2023-11-29 03:27:03 +0400

    Continue to develop

--- script2.js
+++ script2.js
@@ -4,11 +4,12 @@ function showPageContent() {
     const url = 'https://dummyjson.com/products?limit=100&select=title,category,discountPercentage,price,stock,thumbnail';
 
     const container = document.querySelector('main');
+    const searchInput = document.getElementById('searchInput');
+    const categorySelect = document.getElementById('categorySelect');
 
     function showProducts(products) {
+        container.innerHTML = '';
 
-        console.log(products)
-       
         products.forEach(product => {
             const card = document.createElement('div');
             card.setAttribute('class', 'card');
@@ -30,7 +31,6 @@ function showPageContent() {
             const stock = document.createElement('p');
             stock.textContent = 'Current Stock: ' + product.stock;
 
-
             container.appendChild(card);
 
             card.appendChild(thumbnail);
@@ -48,16 +48,42 @@ function showPageContent() {
         window.open('product_Details.html?id=' + product.id, '_blank');
     }
 
-    function handleFetchError() {
-        const error = document.createElement('p');
-        error.textContent = 'Error fetching data, please try again';
-        container.appendChild(error);
-    };
+    function handleFetchError(error) {
+        console.error('Error fetching data:', error);
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = 'Error fetching data, please try again';
+        container.appendChild(errorMessage);
+    }
 
+    function filterProducts(searchKeyword, selectedCategory, products) {
+        return products.filter(product => {
+            const matchesSearchTitle = product.title.toLowerCase().includes(searchKeyword.toLowerCase());
+            const matchesSearchCategory = product.category.toLowerCase().includes(searchKeyword.toLowerCase());
+            const matchesCategory = selectedCategory === '' || product.category.toLowerCase() === selectedCategory.toLowerCase();
+            return (matchesSearchTitle || matchesSearchCategory) && matchesCategory && product.stock > 0;
+        });
+    }
+    
+    function handleSearch() {
+        const searchKeyword = searchInput.value;
+        const selectedCategory = categorySelect.value;
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => showProducts(data.products))
-        .catch(handleFetchError);
-};
+        // Fetch data and show products
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const filteredProducts = filterProducts(searchKeyword, selectedCategory, data.products);
+                showProducts(filteredProducts);
+            })
+            .catch(handleFetchError);
+    }
+
+    // Event listeners for search input and category select
+    searchInput.addEventListener('input', handleSearch);
+    categorySelect.addEventListener('change', handleSearch);
+
+    // Initial fetch and show
+    handleSearch();
+}
+
 
